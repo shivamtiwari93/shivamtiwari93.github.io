cd0bf06d88c57bcb7e5190ca9f1f0f6023720920
shivamtiwari93 <shivamtiwari1011@gmail.com>
2015-02-14 12:11:26 +0530

    formspree used

--- NewWebsiteTrial/js/contact_me.js
+++ NewWebsiteTrial/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://www.scrapbotics.com/shivamtiwari93/mail/contact_me.php",
+                url: "http://formspree.io/iam@shivamtiwari93.in",
                 type: "POST",
 				crossDomain: true,
                 data: {
