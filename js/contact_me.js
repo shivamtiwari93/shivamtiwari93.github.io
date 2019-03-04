36e42b423f76d6818f2a3fc4a0562e751e447940
Shivam Tiwari <shivamtiwari1011@gmail.com>
2019-03-04 17:40:26 +0530

    test text

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Woosh! Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -47,7 +47,7 @@ $(function() {
                     $('#error > .alert-error').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#error > .alert-error')
-                        .append("<strong>Failure in sending message. </strong>");
+                        .append("<strong>Dayum! Failure in sending message. </strong>");
                     $('#error > .alert-error')
                         .append('</div>');
 
