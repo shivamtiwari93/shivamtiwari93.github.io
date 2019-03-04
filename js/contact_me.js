2aecf4685b838a133758217f37422a5d673d9a1f
Shivam Tiwari <shivamtiwari1011@gmail.com>
2019-03-04 17:37:22 +0530

    random trial

--- js/contact_me.js
+++ js/contact_me.js
@@ -43,12 +43,12 @@ $(function() {
                 },
                 error: function() {
                     // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#error').html("<div class='alert alert-success'>");
+                    $('#error > .alert-error').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
+                    $('#error > .alert-error')
+                        .append("<strong>Failure in sending message. </strong>");
+                    $('#error > .alert-error')
                         .append('</div>');
 
                     //clear all fields
