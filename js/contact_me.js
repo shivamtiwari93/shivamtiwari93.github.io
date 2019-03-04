ac187bef41a27064345c2c5ecd1c75237967d8ca
Shivam Tiwari <shivamtiwari1011@gmail.com>
2019-03-04 17:49:19 +0530

    comment out contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/shivamtiwari1011@gmail.com",
+                url: "https://formspree.io/iam@shivamtiwari93.in",
                 type: "POST",
 				crossDomain: true,
                 data: {
